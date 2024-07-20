2c061ca77a4caac54de88a10f8bb6bef85ccb3b8
rameezrz25 <rameezrz25@gamil.com>
2024-07-20 10:57:30 +0530

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="SysBenchInfo",
+    version="0.1.0",
+    description="A package to get system information and benchmark CPU and GPU for all operating systems",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="rameezrz25",
+    author_email="rameezrz25@gmail.com",
+    url="https://github.com/rameezrz25/SysBenchInfo",
+    packages=find_packages(),
+    install_requires=[
+        "psutil",
+        "GPUtil",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
